57ad0a020e67cdf6f9651dccf832e0eb591d9be7
João Alberto Silva Martins <83031228+joaozin11@users.noreply.github.com>
2023-11-08 23:58:36 -0300

    Add files via upload

---
+++ app/src/main/java/br/ulbra/trocatelas/MainActivity.java
@@ -0,0 +1,33 @@
+package br.ulbra.trocatelas;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.*;
+import android.view.*;
+public class MainActivity extends Activity {
+    Button bttelaprincipal, bttela2;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CarregarTela1();
+    }
+    public void CarregarTela1() {
+        setContentView(R.layout.tela1);
+        bttela2 = (Button) findViewById(R.id.bttela2);
+        bttela2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CarregarTela2(); } });
+    }
+
+    public void CarregarTela2() {
+        setContentView(R.layout.tela2);
+        bttelaprincipal = (Button) findViewById (R.id.bttelaprincipal);
+        bttelaprincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CarregarTela1();
+            }
+        });
+    } }
